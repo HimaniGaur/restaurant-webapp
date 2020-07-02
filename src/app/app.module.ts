2c26fc35097e9b5b540bc2f9bc822ff9f8aca9ec
HimaniGaur <himanigaur1998@gmail.com>
2020-07-02 15:16:41 +0530

    dialog box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import {AppRoutingModule} from './app-routing/app-routing.module';
+import {MatDialogModule} from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -16,10 +18,12 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import {DishService} from './services/dish.service';
 import {PromotionService} from './services/promotion.service';
-import {AppRoutingModule} from './app-routing/app-routing.module'
+import {LeaderService} from './services/leader.service';
+
 
 
 @NgModule({
@@ -31,7 +35,8 @@ import {AppRoutingModule} from './app-routing/app-routing.module'
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -42,12 +47,17 @@ import {AppRoutingModule} from './app-routing/app-routing.module'
     MatCardModule,
     MatButtonModule,
     MatListModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule
   ],
   providers: [
     DishService,
-    PromotionService
+    PromotionService,
+    LeaderService
   ],
+  entryComponents: [
+    LoginComponent
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
